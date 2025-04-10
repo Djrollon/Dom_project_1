c8178209172c27290521190ad9f7a894e0d75270
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-04-10 10:23:36 +0100

    DOM_PROJ_2

---
+++ color-changer.js
@@ -0,0 +1,18 @@
+const colorBox = document.getElementById("color-box");
+const changeColorBtn = document.getElementById("change-color-btn");
+
+changeColorBtn.addEventListener("click", function (event) {
+  function getRandomColor() {
+    let randomColor = "#";
+    const letters = "0123456789abcdef";
+    for (let i = 0; i < 6; i++) {
+      randomColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return randomColor;
+  }
+
+  changeColorBtn.addEventListener("click", function () {
+    const newColor = getRandomColor();
+    colorBox.style.backgroundColor = newColor;
+  });
+});
